4a30fd255c1d7049257d7bc309a16c5ecdb38dba
John Doe <johndoe@email.com>
2024-08-01 15:34:26 +0530

    Test Commit

---
+++ cypress/e2e/pages/loginPage/elements.js
@@ -0,0 +1,10 @@
+module.exports = {
+    LOGINPAGE:{
+        LOGIN_PAGE_HEADING: "h1",
+        USERNAME_TEXT: "#username",
+        PASSWORD_TEXT: "#password",
+        REMEMBER_ME_CHECKBOX: "input[type='checkbox']",
+        SIGN_IN_BUTTON: "button[type='submit']",
+        SIGN_UP_LINK: "a[data-test='signup']"
+    }
+}
